e79a9a7ea6d4a657caa65de549ea3871d6146be2
ELITO LENON PRATES <94411573+Lenonp@users.noreply.github.com>
2022-08-17 12:28:11 -0300

    atualização commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from '../Counter/Counter.Js';
+
+function App() {
+  return (
+
+    <>
+      <h1>Hello Word</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
